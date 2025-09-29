de857fbdb78813d718ef82174bcd250d54517712
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-09-29 15:04:53 +0800

    整理文件，添加cmake

---
+++ Examples/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 25473 on 25-9-29.
+//
+#include <iostream>
+#include "VisionSceneVersion.h"
+#include "DatasmithSceneXmlReader.h"
+
+int main()
+{
+    std::cout << "VisionScene version: " << vision_scene::VersionString << "\n";
+    return 0;
+}
---
+++ Src/datasmith_refactor/Public/DatasmithSceneXmlReader.h
@@ -0,0 +1,18 @@
+//
+// Created by 25473 on 25-9-29.
+//
+
+#pragma once
+
+class FDatasmithTextureSampler;
+class FXmlFile;
+class FXmlNode;
+class UTexture;
+struct FLinearColor;
+
+class DatasmithSceneXmlReader {
+
+
+
+};
+
